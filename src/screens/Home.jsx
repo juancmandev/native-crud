682dcaac3dca0bdfe5de2bb6017e07885e71ed68
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-11-03 20:50:39 -0600

    create first components

---
+++ src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { home } from '../styles/styles';
+
+const Home = () => {
+  return (
+    <View style={home.container}>
+      <Text>This is my home</Text>
+    </View>
+  );
+};
+
+export default Home;
