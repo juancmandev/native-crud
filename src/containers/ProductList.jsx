5f3364b5bfc744e18d1c364b6dd236a639677553
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-11-07 17:27:44 -0600

    add product list container

---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const ProductList = () => {
+  return <Text>Product List</Text>;
+};
+
+export default ProductList;
--- src/screens/Tianguis.jsx
+++ src/screens/Tianguis.jsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native';
 import ProductCard from '../components/ProductCard';
+import ProductList from '../containers/ProductList';
 import { home } from '../styles';
 
 const Tianguis = () => {
   return (
     <View style={home.container}>
       <ProductCard />
+      <ProductList />
     </View>
   );
 };
