4becfa9b1478df4d4a5c8bad5b79b065843012cd
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-11-10 12:38:16 -0600

    add user card file

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const UserCard = () => {
+  return (
+    <View>
+      <Text>UserCard</Text>
+    </View>
+  );
+};
+
+export default UserCard;
---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB765zQ56mZElt1BAKLtXFrlc_MwnkS1KM',
+  authDomain: 'native-crud-eb4c1.firebaseapp.com',
+  projectId: 'native-crud-eb4c1',
+  storageBucket: 'native-crud-eb4c1.appspot.com',
+  messagingSenderId: '956080833931',
+  appId: '1:956080833931:web:7d81a40b21e91d6a22db8e',
+};
+
+const app = initializeApp(firebaseConfig);
